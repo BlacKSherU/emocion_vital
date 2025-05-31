fb35789ea59c700f3e9d57001383cfd6c82ff061
BlacKSherU <tramitesgarciamiguel@gmail.com>
2025-05-31 17:34:14 -0400

    muchos cambios realizados la verdad agregue el nuevo panel usuario todavia no funcional elimine el login inicial y cree uno nuevo por motivos que no explicare aparte del register y recuperar usuario

--- main/urls.py
+++ main/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("usuario", views.prueba, name="ususario"),
     path("", views.home, name="home"),
     path("consultas", views.consultas, name="consultas"),
     path("precios", views.precios, name="precios"),
     path("conoce", views.conoce, name="conoce"),
     path("perfil", views.perfil, name="perfil"),
+    path("perfil/formdatos", views.perfil_personal, name="perfil_formdatos"),
+    path("login", views.iniciar_sesion, name="login"),
+    path("registro", views.registro, name="registro"),
+    path("recuperar", views.recuperar, name="recuperar"),
 ]
 
 # Wire up our API using automatic URL routing.
--- main/views.py
+++ main/views.py
@@ -1,25 +1,90 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from api.models import Paciente
+from django.contrib import messages
 
 
-def home(request):
+def home(request: HttpRequest):
     return render(request, "home.html")
 
 
-def consultas(request):
+def consultas(request: HttpRequest):
     return render(request, "consultas.html")
 
 
-def precios(request):
+def precios(request: HttpRequest):
     return render(request, "precios.html")
 
 
-def conoce(request):
+def conoce(request: HttpRequest):
     return render(request, "conoce.html")
 
 
-def perfil(request):
-    return render(request, "perfil.html")
+def perfil_personal(request: HttpRequest):
+    print(request.POST)
+    return redirect("perfil")
 
 
-def prueba(request):
-    return render(request, "usuario.html")
+def perfil(request: HttpRequest):
+
+    paciente = Paciente.objects.get(user=request.user)
+    user = request.user
+    datos = {
+        "nombre": user.first_name,
+        "apellido": user.last_name,
+        "cedula": user.username,
+        "correo": user.email,
+    }
+    return render(request, "panel-psicologa.html", datos)
+
+
+def iniciar_sesion(request: HttpRequest):
+    if request.method == "POST":
+        user = authenticate(
+            request, password=request.POST["password"], username=request.POST["usuario"]
+        )
+        print(request.POST)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect("perfil")
+        messages.error(request, "contraseña invalida", "errror")
+
+    return render(request, "login.html")
+
+
+def registro(request: HttpRequest):
+    if request.method == "POST":
+        print(request.POST)
+        existe = False
+        try:
+            user = User.objects.get(email=request.POST["correo"])
+            messages.error(request, "correo ya registrado cambialo", "errror")
+            existe = False
+        except User.DoesNotExist:
+            existe = True
+
+        try:
+            user = User.objects.get(username=request.POST["cedula"])
+            messages.error(request, "cedula ya registrada cambiala", "errror")
+            existe = False
+        except User.DoesNotExist:
+            if existe:
+                existe = True
+        if existe:
+            user = User.objects.create_user(
+                request.POST["cedula"],
+                request.POST["correo"],
+                request.POST["password"],
+                first_name=request.POST["nombre"],
+                last_name=request.POST["apellido"],
+            )
+            Paciente.objects.create(user=user)
+            return redirect("perfil")
+    return render(request, "registro.html")
+
+
+def recuperar(request: HttpRequest):
+    return render(request, "recuperar.html")
