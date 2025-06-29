9ec247598e777aadf9ddc4e55ad0994cb3e5bf4b
BlacKSherU <tramitesgarciamiguel@gmail.com>
2025-06-29 15:43:03 -0400

    agregue el campo para el docx

--- oswald_api/models.py
+++ oswald_api/models.py
@@ -342,3 +342,9 @@ class Cita(models.Model):
     tipo_consulta = models.CharField(max_length=100, null=True)
     resultado = models.ForeignKey(Resultado, on_delete=models.CASCADE, null=True)
     notas = models.CharField(max_length=200, null=True)
+    historiamedica_archivo = models.FileField(
+        upload_to="historias_medicas/",
+        null=True,
+        blank=True,
+        help_text="Archivo .docx de historia médica",
+    )
